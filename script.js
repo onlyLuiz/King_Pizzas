16181c76f88121749da61fb2ab3a85a16ef5ff32
Luiz Eduardo da Silva Araújo <71404614+onlyLuiz@users.noreply.github.com>
2021-11-23 19:46:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function mouFocus(a) {
+    a.classList.add('focus');
+}
+
+function mouBlur(a) {
+    a.classList.remove('focus');
+}
+
+
+function naOver(a) {
+    a.classList.add('navOver');
+}
+
+function navOut(a) {
+    a.classList.remove('navOver');
+}
+
+function footOver(a) {
+    a.classList.add('footOver');
+}
+
+function footOut(a) {
+    a.classList.remove('footOver');
+}
+
+function linkOver(a) {
+    a.classList.add('linkOver');
+}
+
+function linkOut(a) {
+    a.classList.remove('linkOver');
+}
+
+function linkClick(a){
+    a.classList.add('linkClick');
+}
